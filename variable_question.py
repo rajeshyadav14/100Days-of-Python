c0ddfba944079108751ae0c497deb5f847719993
hiddenxworld1 <hiddenxworld@gmail.com>
2026-04-16 00:32:12 +0530

    Updated variable_question.py

---
+++ variable_question.py
@@ -0,0 +1,67 @@
+
+#1. Store your name in a variable and print it.
+
+name = "Rajesh Yadav"
+print(name)
+
+
+#2 Store two numbers in variables and print their sum 
+
+sum1 = 18
+sum2 = 24
+print(sum1+sum2)
+
+#3 Store length and width in variables and calculate the area of a rectangle 
+
+length = 15
+width = 20
+area = length * width
+print(area)
+
+#5 Store a string in a variable and print its square 
+
+value = 25
+square = value*value
+print(square)
+
+#4 Store a number in a varible and print it in uppercase 
+
+text = "hidden" 
+print(text.upper())
+
+#6 Swap two variables without using a third variable
+
+a = 10
+b = 8
+a , b = b , a
+print(a,b)
+
+#7 Convert temperature from Celsius to Fahrenheit using variables
+
+celsius = 25
+fehrenheit = (9/5*celsius)+32
+print(fehrenheit)
+
+
+#8 Store a string and print its length 
+
+text = "NameString"
+print(len(text))
+
+#9 Store a number and check if it is even or odd 
+
+num = 2 
+if(num%2==0):
+    print("Its a even")
+else:
+    print("Its a odd")
+
+
+#10 Store marks in a variable and print “Pass” or “Fail” 
+
+marks = 34
+
+if (marks >= 35):
+    print("pass")
+else:
+    print("fail")
