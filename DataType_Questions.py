4eb27f1ef6c6693b87ac22c00371792ce0540cb7
hiddenxworld1 <hiddenxworld@gmail.com>
2026-04-16 23:03:03 +0530

    Updated dataType.py

---
+++ DataType_Questions.py
@@ -0,0 +1,37 @@
+
+#Integer
+
+#Store an integer and print its type
+
+x = 24
+print(x,type(x))
+
+#Add two integer variables and print the result
+a = 10
+b = 8
+c = a + b 
+print("Addition of Two Interger:",c)
+
+#Take an integer input and print its square
+
+d = 2 
+squ = d * d 
+print(squ)
+
+#Float
+
+#Store a float value and print its type
+
+num = 3.14
+print(num,type(num))
+print(int(num))
+
+#Convert an integer into float and print it
+
+num1 = 24
+print(float(num1),type(num1))
+
+#Add a float and integer and print the result
+
+add = num + num1
+print(add,type(add))
