2c4164a6ca5c9747af208c8f6bf802bc8af7d8f1
hiddenxworld1 <hiddenxworld@gmail.com>
2026-04-16 20:51:02 +0530

    add

---
+++ string.py
@@ -0,0 +1,21 @@
+
+#Python Program to Demonstration of String
+
+#Creating a String with single Quotes
+
+str1 = 'Rajesh Yadav'
+print("String with  the use of Single Quotes:",str1)
+print("Type of Str1 = ",type(str1))
+
+#Creating a String with Double Quotes
+
+str2 = "Hello World!"
+print("String with the use of double Quotes:",str2)
+print("Type of Str2 = ",type(str2))
+
+#Creating a String with Triple Quotes with MultiLiner 
+
+str3 = '''Hello
+                World!'''
+print("String with the use of triple Quotes:",str3)
+print("Type of Str3 = ",type(str3) )
