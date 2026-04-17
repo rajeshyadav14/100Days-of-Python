0bf894c5825e5aa730bd056c12c2ec7a99cd6e3b
hiddenxworld1 <hiddenxworld@gmail.com>
2026-04-17 23:46:43 +0530

    updated

--- input_output.py
+++ input_output.py
@@ -31,8 +31,61 @@ print("Square of",sqr,"is",res)
 '''
 
 #Take temperature in Celsius and convert it to Fahrenheit
+'''
+celsius = float(input("Enter a Celsius: "))
+fahrenheit = celsius * 9/5 + 32
+print("Your are Celsius is",celsius,"after convert into fahrenheit is",fahrenheit)
+'''
 
-cel = float(input("Enter a Celsius: "))
-fahr = cel * 9/5 + 32
-print("Your are Celsius is",cel,"after convert into fahrenheit is",fahr)
 
+#Take two numbers (as input) and print: Sum, Difference, Product 
+'''
+a = float(input("Enter Number 1: "))
+b = float(input("Enter Number 2: "))
+print(f"Sum:{a+b}")
+print(f"Difference:{a-b}")
+print(f"Product:{a*b}")
+'''
+
+#Take a decimal number and convert it to an integer, then print it
+'''
+num = float(input("Enter a Decimal Number: "))
+res = round(num)
+print(f"After Converting Decimal to Interger Value is: {res}")
+'''
+
+#Take name and birth year → calculate age
+'''
+name = input("Enter name: ")
+birthYear = int(input("Enter Birth Year: "))
+currentYear = int(input("Enter Current Year: "))
+currentAge = currentYear - birthYear
+
+print(f"Your Name is:{name}")
+print(f"Your Current Age is:{currentAge}")
+'''
+
+
+#Take 3 numbers and print their average
+
+num11 = float(input("Enter Number 1: "))
+num12 = float(input("Enter Number 2: "))
+num13 = float(input("Enter Number 3: "))
+average = (num11+num12+num13)/3
+print(f"Average:{average}")
+
+#Take length and width → print area of rectangle
+
+length = float(input("Enter length: "))
+width = float(input("Enter Width: "))
+area = length * width
+print(f"Area of Rectange is {area}")
+
+#Take a number and print:
+#Double: __
+#Triple: __
+
+num14 = float(input("Enter a number: "))
+
+print(f"Double: {num14 * 2}")
+print(f"Triple: {num14 * 3}")
