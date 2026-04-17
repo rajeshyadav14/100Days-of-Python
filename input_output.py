87677dcd7a59ecd001ee119a91d02ee99a03b32f
hiddenxworld1 <hiddenxworld@gmail.com>
2026-04-17 16:06:20 +0530

    updated

--- input_output.py
+++ input_output.py
@@ -1,6 +1,38 @@
 
-name = input("Enter your Name:")
-age = int(input("Enter your Age:"))
+#Take a name as input and print:
 
-print("Your name is:",name)
-print("Your age is:",age)
+"""
+userName = input("Enter Name:")
+print("Hello",userName,"Welcome to Hacker Lobby!")
+"""
+
+#Take two numbers as input and print their sum
+'''
+num1 = int(input("Enter Number1: "))
+num2 = int(input("Enter Number2: "))
+total = num1 + num2 
+print("Sum of num1 and num2 is:",total)
+
+'''
+
+#Take age as input and print:
+'''
+userAge = int(input("Enter Age: "))
+print("You are",userAge,"years old.")
+'''
+
+#Take a number as input and print its square
+
+'''
+sqr = int(input("Enter a Number for getting Square: "))
+res = sqr ** 2 
+print("Square of",sqr,"is",res)
+
+'''
+
+#Take temperature in Celsius and convert it to Fahrenheit
+
+cel = float(input("Enter a Celsius: "))
+fahr = cel * 9/5 + 32
+print("Your are Celsius is",cel,"after convert into fahrenheit is",fahr)
+
